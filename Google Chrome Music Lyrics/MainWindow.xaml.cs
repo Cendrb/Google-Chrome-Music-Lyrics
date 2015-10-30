0004aff75ee2bdd089260a60f3abcb69b667e62b
Čeněk Drbal <cendrb@gmail.com>
2015-10-30 15:38:51 +0100

    first commit /96/

---
+++ Google Chrome Music Lyrics/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Google_Chrome_Music_Lyrics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private HttpListener listener = new HttpListener();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            listener.Prefixes.Add("http://127.0.0.1:6923/");
+
+            listener.Start();
+
+            Run();
+        }
+
+        public void Run()
+        {
+            ThreadPool.QueueUserWorkItem((o) =>
+                {
+                    try
+                    {
+                        while (listener.IsListening)
+                        {
+                            ThreadPool.QueueUserWorkItem((c) =>
+                            {
+                                HttpListenerContext context = c as HttpListenerContext;
+                                try
+                                {
+                                    byte[] data = new byte[1024];
+                                    context.Request.InputStream.Read(data, 0, data.Length);
+                                    string rawString = Encoding.UTF8.GetString(data).Replace("\0", String.Empty);
+                                    SongData song = SongData.Parse(rawString);
+                                    song.FetchLyrics();
+
+                                    Dispatcher.Invoke(() => loadFromSongToGUI(song));
+                                    notifyAndroidDevice(song);
+
+                                    byte[] buf = Encoding.UTF8.GetBytes("penis");
+                                    context.Response.ContentLength64 = buf.Length;
+                                    context.Response.OutputStream.Write(buf, 0, buf.Length);  
+                                }
+                                catch (Exception e)
+                                {
+
+                                }
+                                finally
+                                {
+                                    context.Response.OutputStream.Close();
+                                }
+                            }, listener.GetContext());
+                        }
+                    }
+                    catch (Exception e)
+                    {
+
+                    }
+                });
+        }
+
+        private void loadFromSongToGUI(SongData song)
+        {
+            titleText.Content = song.Title;
+            artistAlbumText.Content = song.Artist + " - " + song.Album;
+            lyricsText.Content = song.Lyrics;
+            Topmost = true;
+            Topmost = false;
+        }
+
+        private void notifyAndroidDevice(SongData song)
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+                client.Connect(IPAddress.Parse("192.168.1.92"), 6969);
+                StreamWriter writer = new StreamWriter(client.GetStream());
+                writer.WriteLine(song.Title);
+                writer.WriteLine(song.Artist);
+                writer.WriteLine(song.Album);
+                writer.WriteLine(song.Lyrics);
+                writer.Close();
+                client.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
---
+++ Google Chrome Music Lyrics/SongData.cs
@@ -0,0 +1,70 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Google_Chrome_Music_Lyrics
+{
+    class SongData
+    {
+        public string Artist { get; private set; }
+        public string Album { get; private set; }
+        public string Title { get; private set; }
+
+        public string Lyrics
+        {
+            get
+            {
+                if (lyrics == null)
+                    throw new InvalidOperationException("No lyrics fetched. Need to call FetchLyrics() method first.");
+                else
+                    return lyrics;
+            }
+        }
+        private string lyrics;
+
+        public SongData(string artist, string album, string title)
+        {
+            Artist = artist;
+            Album = album;
+            Title = title;
+        }
+
+        public static SongData Parse(string rawString)
+        {
+            string[] parts = rawString.Split(';');
+            return new SongData(parts[0], parts[1], parts[2]);
+        }
+
+        public bool FetchLyrics()
+        {
+            string title = Title.Replace(".", "").Replace("'", "").Replace("’", "").Replace("-", "").Replace("(", "").Replace(")", "").Replace("!", "");
+            string artist = Artist.Replace(".", "").Replace("'", "").Replace("’", "").Replace("-", "").Replace("(", "").Replace(")", "").Replace("!", "");
+            string url = String.Format("http://www.azlyrics.com/lyrics/{0}/{1}.html", artist, title).Replace(" ", "").ToLower();
+            WebRequest request = WebRequest.Create(url);
+
+            try
+            {
+                WebResponse response = request.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                HtmlDocument document = new HtmlDocument();
+                document.LoadHtml(reader.ReadToEnd());
+                HtmlNode result = document.DocumentNode.SelectNodes("//div[@class='container main-page']/div[@class='row']/div[@class='col-xs-12 col-lg-8 text-center']/div[6]")[0];
+                lyrics = result.InnerText;
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+    }
+}
